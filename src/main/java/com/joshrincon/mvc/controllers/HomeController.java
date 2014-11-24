e647376a209f3f9aa2e6ab876e2a34466884fba5
Josh Rincon <memonkey@gmail.com>
2014-11-23 23:55:18 -0800

    junit tests now broken... pushing so i can work remotely tomorrow

--- src/main/java/com/joshrincon/mvc/controllers/HomeController.java
+++ src/main/java/com/joshrincon/mvc/controllers/HomeController.java
@@ -1,13 +1,15 @@
 package com.joshrincon.mvc.controllers;
 
-import org.apache.log4j.Logger;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
-    private static Logger logger = Logger.getLogger(HomeController.class);
+    private static Logger logger = LogManager.getLogger(HomeController.class.getName());
 
     @RequestMapping("/")
     public String showHome() {
--- src/test/java/com/joshrincon/mvc/tests/UserDAOTests.java
+++ src/test/java/com/joshrincon/mvc/tests/UserDAOTests.java
@@ -1,23 +1,46 @@
 package com.joshrincon.mvc.tests;
 
-import org.junit.Assert;
+import com.joshrincon.mvc.dao.User;
+import com.joshrincon.mvc.dao.UsersDAO;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import javax.sql.DataSource;
+
+import static org.junit.Assert.*;
+
 @ActiveProfiles("development")
-@ContextConfiguration(locations = {"classpath:../webapp/WEB-INF/dao-context.xml",
-                                    "classpath:../webapp/WEB-INF/security-context.xml",
-                                    "classpath:com/joshrincon/mvc/config/datasource.xml"})
+@ContextConfiguration(locations = {"classpath:com/joshrincon/mvc/config/datasource.xml",
+                                    "classpath:../webapp/WEB-INF/dao-context.xml",
+                                    "classpath:../webapp/WEB-INF/security-context.xml"})
 // says how we're gonna run the above context configs
 @RunWith(SpringJUnit4ClassRunner.class)
 public class UserDAOTests {
 
+    @Autowired
+    private UsersDAO usersDAO;
+
+    @Autowired
+    private DataSource dataSource;
+
+    // run this before my tests
+    @Before
+    public void init() {
+        JdbcTemplate jdbc = new JdbcTemplate(dataSource);
+        jdbc.execute("delete from users");
+        jdbc.execute("delete from authorities");
+    }
+
     @Test
     public void testCreateUser() {
-        Assert.assertEquals("Dummy test", 1, 1);
+        User user = new User("testuser", "passwordtest", "testuser@tester.com", true, "user");
+        assertTrue(usersDAO.create(user));
     }
 
 }
